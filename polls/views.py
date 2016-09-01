fa39bf91b9f38205a2eefe76a20d3f012f1e5b55
Gnaneshwar Rao Ayanala <gayanala@unomaha.edu>
2016-09-01 03:20:52 -0500

    Part3 - added templates for views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,16 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+	
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
